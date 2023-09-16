3187d85f0487aff8e55d7382cac74830cc17474b
Backwasch <Birkan1010@hotmail.de>
2023-09-16 18:58:14 +0200

    Bubblesort using functions and *args

--- Bubblesort/main.py
+++ Bubblesort/main.py
@@ -1,11 +1,14 @@
-numbers = [40, 24, 60, 15, 52, 98, 46, 7, 27, 40, -5, -2, 69]
 
-for i in range (len(numbers)):
-    for x in range(0, len(numbers)-1):
-        if numbers[x] > numbers [x+1]:
-            numbers [x], numbers [x+1] = numbers [x+1], numbers[x]
-            print(numbers)
+def bubblesort(*args):
+    print(args)
+    numbers = list(args)
+    for i in range (len(numbers)):
+        for x in range(0, len(numbers)-1):
+            if numbers[x] > numbers [x+1]:
+                numbers [x], numbers [x+1] = numbers [x+1], numbers[x]
+                print(numbers)
+    return(numbers)
+
+print(bubblesort(1,55,11,77,8,5,1,23,9,87,458,48,45,45964,89,489,489,4))
 
 
-
-print("Sorted Numbers: " + str(numbers))
