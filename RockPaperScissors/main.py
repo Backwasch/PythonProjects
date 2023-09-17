2fa1f124009a4bc9734baebfb67ae34d75148a32
Backwasch <Birkan1010@hotmail.de>
2023-09-17 15:24:01 +0200

    RockPaperScissors: Added Replay and Counter

--- RockPaperScissors/main.py
+++ RockPaperScissors/main.py
@@ -1,20 +1,29 @@
 import random
 import time
 
-choice = ["Rock","Paper","Scissors"]
-player = ""
-while player not in choice:
-    player = input("Rock,Paper,Scissor?: ")
+player_count = enemy_count = 0
 
-enemy = random.choice(choice)
-for x in choice:
-    print(str(x))
-    time.sleep(0.5)
+while (True):
+    choice = ["Rock","Paper","Scissors"]
+    player = ""
+    while player not in choice:
+        player = input("Rock,Paper,Scissor?: ")
 
-if player == enemy:
-    print("Draw! \nThe Enemy also had " + enemy +"!")
-elif ((enemy == "Rock" and player == "Scissors") or (enemy == "Paper" and player == "Rock") or (enemy == "Scissors" and player == "Paper")):
-    print("You Lost! \nThe Enemy had " + enemy +"!")
-else:
-    print("You Won! \nThe Enemy had " + enemy +"!")
+    enemy = random.choice(choice)
+    for x in choice:
+        print(str(x))
+        time.sleep(0.5)
 
+    if player == enemy:
+        print("Draw! \nThe Enemy also had " + enemy +"!")
+    elif ((enemy == "Rock" and player == "Scissors") or (enemy == "Paper" and player == "Rock") or (enemy == "Scissors" and player == "Paper")):
+        print("You Lost! \nThe Enemy had " + enemy +"!")
+        enemy_count += 1
+    else:
+        print("You Won! \nThe Enemy had " + enemy +"!")
+        player_count =+ 1
+    print("\nEnemy: " +  str(enemy_count) +",Player: " +str(player_count))
+    askPlayer = input("Do you wanna player again? y/n: ")
+    if (askPlayer != "y"):
+        print("Nice game, bye!")
+        break
